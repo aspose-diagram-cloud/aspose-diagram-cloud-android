feb41b269579d546e83afa17509e50d15eb784ee
Gordon Ge <feng.ge@aspose.com>
2019-01-21 11:11:14 +0800

    first commit

---
+++ src/androidTest/java/com/aspose/cloud/diagram/api/DiagramApiUtil.java
@@ -0,0 +1,52 @@
+package com.aspose.cloud.diagram.api;
+
+import com.aspose.cloud.diagram.client.ApiClient;
+import com.aspose.cloud.diagram.client.ApiException;
+import com.aspose.cloud.diagram.model.AccessTokenResponse;
+
+public class DiagramApiUtil {
+	private static String accesstoken;
+	private static String grantType = "client_credentials";
+	private static String clientId = "xxxxxxxx";
+	private static String clientSecret = "xxxxxxxxxx";
+
+	public static String GetGrantType() {
+		return grantType;
+	}
+
+	public static String GetClientId() {
+		return clientId;
+	}
+
+	public static String GetClientSecret() {
+		return clientSecret;
+	}
+
+	public static String GetAccessToken() {
+		try {
+			if (accesstoken == null || accesstoken == "") {
+				System.out.println("--------------------------");
+				ApiClient apiClient = new ApiClient();
+				apiClient.setBasePath("https://api.aspose.cloud/");
+				OAuthApi oauth2 = new OAuthApi();
+				oauth2.setApiClient(apiClient);
+				AccessTokenResponse accessTokenResponse = oauth2.oAuthPost(grantType, clientId, clientSecret);
+				accesstoken = accessTokenResponse.getAccessToken();
+			}
+
+		} catch (ApiException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return accesstoken;
+	}
+
+	public static ApiClient Ready() {
+		DiagramFileApi apiTask = new DiagramFileApi();
+		ApiClient apiClient = new ApiClient();
+		apiClient.setBasePath("https://api.aspose.cloud/v1.1");
+		apiClient.addDefaultHeader("Authorization", "Bearer " + GetAccessToken());
+		apiTask.setApiClient(apiClient);
+		return apiClient;
+	}
+}
